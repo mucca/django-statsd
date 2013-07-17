7207da0465ad97529e0514bcaae2a1b70f584398
Saverio <saver.mucci@gmail.com>
2013-07-17 18:58:15 +0200

    comment all

--- django_statsd/pyelasticsearch.py
+++ django_statsd/pyelasticsearch.py
@@ -1,22 +1,22 @@
-from __future__ import absolute_import
-from django_statsd.utils import get_timer
-import django_statsd
+# from __future__ import absolute_import
+# from django_statsd.utils import get_timer
+# import django_statsd
 
-try:
-    import pyelasticsearch
+# try:
+#     import pyelasticsearch
 
-    class StatsdElasticSearch(pyelasticsearch.ElasticSearch):
-        def search(self, *args, **kwargs):
-            timer = get_timer('totals.pyelasticsearch')
-            timer.start()
-            with django_statsd.with_('pyelasticsearch.search'):
-                result = origElasticSearch.search(self, *args, **kwargs)
-            timer.stop()
-            return result
+#     class StatsdElasticSearch(pyelasticsearch.ElasticSearch):
+#         def search(self, *args, **kwargs):
+#             timer = get_timer('totals.pyelasticsearch')
+#             timer.start()
+#             with django_statsd.with_('pyelasticsearch.search'):
+#                 result = origElasticSearch.search(self, *args, **kwargs)
+#             timer.stop()
+#             return result
 
-    origElasticSearch = None
-    if not issubclass(pyelasticsearch.ElasticSearch, StatsdElasticSearch):
-        origElasticSearch = pyelasticsearch.ElasticSearch
-        pyelasticsearch.ElasticSearch = StatsdElasticSearch
-except ImportError:
-    pass
+#     origElasticSearch = None
+#     if not issubclass(pyelasticsearch.ElasticSearch, StatsdElasticSearch):
+#         origElasticSearch = pyelasticsearch.ElasticSearch
+#         pyelasticsearch.ElasticSearch = StatsdElasticSearch
+# except ImportError:
+#     pass
--- django_statsd/redis.py
+++ django_statsd/redis.py
@@ -1,26 +1,26 @@
-from __future__ import absolute_import
-from django_statsd.utils import get_timer
-import django_statsd
+# from __future__ import absolute_import
+# from django_statsd.utils import get_timer
+# import django_statsd
 
 
-try:
-    import redis
+# try:
+#     import redis
 
-    class StatsdRedis(redis.Redis):
-        def execute_command(self, func_name, *args, **kwargs):
-            timer = get_timer('totals.redis.%s' % func_name.lower())
-            timer.start()
-            with django_statsd.with_('redis.%s' % func_name.lower()):
-                result = origRedis.execute_command(self, func_name, *args,
-                                                   **kwargs)
-            timer.stop()
-            return result
+#     class StatsdRedis(redis.Redis):
+#         def execute_command(self, func_name, *args, **kwargs):
+#             timer = get_timer('totals.redis.%s' % func_name.lower())
+#             timer.start()
+#             with django_statsd.with_('redis.%s' % func_name.lower()):
+#                 result = origRedis.execute_command(self, func_name, *args,
+#                                                    **kwargs)
+#             timer.stop()
+#             return result
 
-    origRedis = None
-    # NOTE issubclass is true if both are the same class
-    if not issubclass(redis.Redis, StatsdRedis):
-        origRedis = redis.Redis
-        redis.Redis = StatsdRedis
-except ImportError:
-    pass
+#     origRedis = None
+#     # NOTE issubclass is true if both are the same class
+#     if not issubclass(redis.Redis, StatsdRedis):
+#         origRedis = redis.Redis
+#         redis.Redis = StatsdRedis
+# except ImportError:
+#     pass
 
